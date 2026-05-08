36a8be47c5f24f6f63314157297f8c707729bc1f
dsfalcon <163556989+devesh-singhal@users.noreply.github.com>
2026-05-08 15:50:42 +0530

    git push origin demopage

---
+++ src/main/java/com/qa/opencart/pges/DemoPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pges;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+	
+	By loc = By.id("demo");
+	public void getPage() {
+		System.out.println("click on demo");
+	}
+
+}
