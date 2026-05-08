d94516f67c18135a12a388b2a72c4a7336d0a3ae
dsfalcon <163556989+devesh-singhal@users.noreply.github.com>
2026-05-08 16:05:30 +0530

    updated code as per review comments

--- src/main/java/com/qa/opencart/pges/DemoPage.java
+++ src/main/java/com/qa/opencart/pges/DemoPage.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.By;
 
 public class DemoPage {
 	
-	By loc = By.id("demo");
-	public void getPage() {
+	By loc = By.cssSelector(".demo");
+	public int getPage() {
 		System.out.println("click on demo");
+		return 0;
 	}
 
 }
